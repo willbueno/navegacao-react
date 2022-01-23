e83c315d26c4cb31b1e4d35037ab75b9aa44483d
William Rocha <william.bueno.rocha@gmail.com>
2022-01-23 00:38:19 -0300

    added page not found

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -6,14 +6,15 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../../views/examples/Home'
 import Param from '../../views/examples/Param'
 import About from '../../views/examples/About'
+import NotFound from '../../views/examples/NotFound'
 
 const Content = props => (
     <main className='Content'>
         <Routes>
             <Route path='/about' element={<About />} />
             <Route path='/param/:id' element={<Param />} />
-            <Route path='/about' element={<About />} />
             <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
         </Routes>
     </main>
 )
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -19,6 +19,9 @@ const Menu = props => (
                 <li>
                     <Link to='/about'>About</Link>
                 </li>
+                <li>
+                    <Link to='/notexist'>Not exist</Link>
+                </li>
             </ul>
         </nav>
     </div>
